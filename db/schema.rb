8307c54c150ca751dea8b589ae97870bd6355a98
ArthurYaxiongWang <arthurwyx@gmail.com>
2024-06-26 15:48:47 -0500

    Fixed the minor problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_25_205926) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_25_204806) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
